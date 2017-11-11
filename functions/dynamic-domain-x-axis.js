e64588578a06b10fb5a5bd2d3062c8f08d118478
tctx <tommy.campbell@ymail.com>
2017-11-11 13:43:19 -0600

    Allow dynamically setting domain values for simple charts when setting env variables, add dynamic-domain function

---
+++ functions/dynamic-domain-x-axis.js
@@ -0,0 +1,5 @@
+// Domain needs to be passed in as an array
+// For simple bar charts (for example), we can simply map the index of each value in an our data array
+
+var dataDomain = [];
+var xDomain = data.map(function(i){return dataDomain.push(i); }); // Pass this variable to our x-axis domain as appropriate
